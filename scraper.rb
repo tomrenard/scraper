2baba25a017a669ebd2f7ddbca05c899c91baf42
Tom Renard <tom.renard@zoho.com>
2020-11-25 20:16:15 +0100

    scraping tool

---
+++ scraper.rb
@@ -0,0 +1,50 @@
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
+def scrape_urls
+  pages_urls = []
+  i = 1
+  until i > 34 do
+    pages_url = "https://www.emploi-collectivites.fr/esp_entreprises/annuaire_entreprises/mairie-collectivite-searchresult.asp?motclef=&qu=A&regID=-1&Page=#{i += 1}"
+    pages_urls << pages_url
+  end
+  mairies_urls = []
+  pages_urls.each do |pages_url|
+    html = open(pages_url)
+    doc = Nokogiri::HTML(html)
+    mairies = doc.css('.line').search('td:nth-child(n)').css('.fontBlue>a')
+    mairies.each do |event|
+      url = event.attribute('href').value
+      mairies_urls << url
+    end
+    scrape_mail(mairies_urls)
+  end
+end
+
+def scrape_mail(mairies_urls)
+  mairies_mails = []
+  mairies_urls.each do |mairies_url|
+    url = "#{mairies_url}"
+    html = open(url)
+    doc = Nokogiri::HTML(html)
+    td = doc.css('.mairieTable').search(':nth-child(n)').search('td:nth-child(n)').text
+    if td.include?('email')
+      first_s = td.split('email :')
+      second_s = first_s[1].split('Region')
+      mail = second_s[0]
+    else
+      mail = "Mail not included"
+    end
+    mairies_mails << mail
+  end
+  p mairies_mails
+end
+
+scrape_urls
+
+# File.open('mairies.txt', 'w') do |f|
+#   mairies_mails.each do |ch|
+#     f.write("#{ch}\n")
+#   end
+# end
