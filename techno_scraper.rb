51df626e57d367ed1e535ffbc0958870f5faae3c
Tom Renard <tom.renard@zoho.com>
2020-11-28 19:28:11 +0100

    finish techno scraper

--- techno_scraper.rb
+++ techno_scraper.rb
@@ -30,21 +30,30 @@ def scrape_techno(tracks_urls)
     artist = doc.css('#profile_title>span>span').text.gsub("\n", "").gsub("  ", "")
     title = doc.css('#profile_title>span:nth-child(2n)').text.gsub("\n", "").gsub("  ", "")
 
-
     photo_urls = []
-    photo_links =
+    photo_links = doc.css('.image_gallery>a>span:nth-child(2n)>img')
+    photo_links.each do |photo_link|
+      photo_url = photo_link.attribute('src').value
+      photo_urls << "#{photo_url}"
+    end
 
     video_urls = []
-    video_links =
+    video_links = doc.css('#youtube_player_wrapper').search('#youtube_player_placeholder')
+    # p video_links
+    video_links.each do |video_link|
+      video_url = video_link.attribute('data-video-ids').value.split(',')[0]
+      video_urls << "https://www.youtube.com/watch?v=#{video_url}"
+    end
 
     tracks_info = {
       title: title,
       artist: artist,
-      # photo_link: photo_link,
-      # video_link: video_link
+      photo_link: photo_urls[0],
+      video_link: video_urls[0]
     }
     techno_tracks << tracks_info
   end
+  p techno_tracks
 end
 
 scrape_urls
