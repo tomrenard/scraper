40fc4abb494d250458d0c34426b8cf0ee5b89d92
Tom Renard <tom.renard@zoho.com>
2020-12-11 18:44:31 +0100

    mechanize testing

---
+++ mechanize_test.rb
@@ -0,0 +1,16 @@
+require 'nokogiri'
+require 'open-uri'
+require 'mechanize'
+
+agent = Mechanize.new
+page = agent.get('https://m.facebook.com/')
+agent.user_agent_alias = 'Android'
+form_a = page.form
+form_a.email = "renardtom@hotmail.fr"
+form_a.pass =  ""
+page = agent.submit(form_a)
+link_a = page.links[1].href
+url = "https://m.facebook.com/#{link_a}"
+html = open(url)
+doc = Nokogiri::HTML(html)
+p doc
