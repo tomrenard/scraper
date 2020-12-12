cf7f50a231967b5635bd187b96caa60a43254d76
Tom Renard <tom.renard@zoho.com>
2020-12-12 12:14:01 +0100

    fb scraping

--- mechanize_test.rb
+++ mechanize_test.rb
@@ -3,11 +3,11 @@ require 'open-uri'
 require 'mechanize'
 
 agent = Mechanize.new
-page = agent.get('https://m.facebook.com/')
+page = agent.get('https://facebook.com/?_fb_noscript=1')
 agent.user_agent_alias = 'Android'
 form_a = page.form
 form_a.email = "renardtom@hotmail.fr"
-form_a.pass =  ""
+form_a.pass =  "57m9dx8Z+zorro"
 page = agent.submit(form_a)
 link_a = page.links[1].href
 url = "https://m.facebook.com/#{link_a}"
