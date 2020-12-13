8f2c98f5d204bd6ff73605310508875bb2b58a3f
Tom Renard <tom.renard@zoho.com>
2020-12-14 00:16:38 +0100

    security data correction

--- mechanize_test.rb
+++ mechanize_test.rb
@@ -6,8 +6,8 @@ agent = Mechanize.new
 page = agent.get('https://facebook.com/?_fb_noscript=1')
 agent.user_agent_alias = 'Android'
 form_a = page.form
-form_a.email = "renardtom@hotmail.fr"
-form_a.pass =  "57m9dx8Z+zorro"
+form_a.email = ""
+form_a.pass =  ""
 page = agent.submit(form_a)
 link_a = page.links[1].href
 url = "https://m.facebook.com/#{link_a}"
