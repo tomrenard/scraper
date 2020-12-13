77e000fc21fb65cc3b22b7a20ba025b47d2fbaa7
Tom Renard <tom.renard@zoho.com>
2020-12-13 17:30:15 +0100

    voyages incentive wip

--- reves.rb
+++ reves.rb
@@ -2,28 +2,27 @@ require 'nokogiri'
 require 'open-uri'
 
 def scrape_urls
-  url = "https://www.capdel.fr/seminaire-dentreprise"
+  url = "https://www.capdel.fr/voyage-incentive"
   html = open(url)
   doc = Nokogiri::HTML(html)
-  sem_links = []
-  sem = doc.css('.info-wrap')
-  sem.each do |s|
+  inc_links = []
+  inc = doc.css('.info-wrap')
+  inc.each do |s|
       link = s.attribute('href').value
-      sem_links << link
+      inc_links << link
     end
-  scrape_sem(sem_links)
+  scrape_sem(inc_links)
 end
 
-
-def scrape_sem(sem_links)
-  sems = []
-  sem_links.each do |sem_link|
+def scrape_sem(inc_links)
+  incs = []
+  inc_links.each do |sem_link|
     url = "https://www.capdel.fr/#{sem_link}"
     html = open(url)
     doc = Nokogiri::HTML(html)
-    category = doc.css(".category-title").text
-    title = doc.css(".title").text
-    details = doc.css(".details").text
+    category = doc.css(".category-title").text.lstrip
+    title = doc.css(".title").text.lstrip
+    details = doc.css(".details").text.lstrip
 
     if_pin = doc.css(".details").search("li").search(".icon-pin")
     if_timer = doc.css(".details").search("li").search(".icon-timer")
@@ -31,27 +30,41 @@ def scrape_sem(sem_links)
     if_calendar = doc.css(".details").search("li").search(".icon-calendar")
     if_language = doc.css(".details").search("li").search(".icon-language")
 
-    loc = doc.css(".details>:first-child").text
+    loc = doc.css(".details>:first-child").text.lstrip
 
     duration = doc.css(".details>:nth-child(2)").text
-    if_timer.empty? ? duration = "Durée à confirmer" : duration = duration
+    if_timer.empty? ? duration = "Durée à confirmer" : duration = duration.lstrip
 
-    person1 = doc.css(".details>:nth-child(2)").text
-    person2 = doc.css(".details>:nth-child(3)").text
+    person1 = doc.css(".details>:nth-child(2)").text.lstrip
+    person2 = doc.css(".details>:nth-child(3)").text.lstrip
     if_timer.empty? ? person = person1 : person = person2
 
-    period1 = doc.css(".details>:nth-child(3)").text
-    period2 = doc.css(".details>:nth-child(4)").text
+    period1 = doc.css(".details>:nth-child(3)").text.lstrip
+    period2 = doc.css(".details>:nth-child(4)").text.lstrip
     if_timer.empty? ? period = period1 : period = period2
 
-    lang1 = doc.css(".details>:nth-child(4)").text
-    lang2 = doc.css(".details>:nth-child(5)").text
+    lang1 = doc.css(".details>:nth-child(4)").text.lstrip
+    lang2 = doc.css(".details>:nth-child(5)").text.lstrip
     if_timer.empty? ? lang = lang1 : lang = lang2
 
-    description = doc.css(".description-text").text
-    pros = doc.css(".card-body>p").text
+    description = doc.css(".description-text").text.lstrip
+    pros = doc.css(".card-body>p").text.lstrip
 
-    p pros
+    pic_url = "https://source.unsplash.com/1600x900/?#{loc},tourism"
+
+    incentive_info = {
+      category: category,
+      title: title,
+      loc: loc,
+      duration: duration,
+      person: person,
+      period: period,
+      lang: lang,
+      description: description,
+      pic_url: pic_url
+    }
+    incs << incentive_info
+    p incentive_info
   end
 end
 
