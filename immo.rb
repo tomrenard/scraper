c427241648f4f2a99221121b579e95b384b6d406
Tom Renard <tom.renard@zoho.com>
2020-12-15 18:40:43 +0100

    immo correction

--- immo.rb
+++ immo.rb
@@ -13,15 +13,15 @@ form_name.account = ""
 form_name.password = ""
 button = form_name.button
 test1 = form_name.submit
-p test1
+# p test1
 
 
 
 
 # page = agent.submit(form_name, button)
-# browser = Watir::Browser.new
-# browser.goto('http://riviera.prescripteurs.axessia.net')
-# p browser.forms
+browser = Watir::Browser.new
+browser.goto('http://riviera.prescripteurs.axessia.net')
+p browser.forms
 # browser.text_field(data_test: 'email').set '3113'
 # browser.text_field(data_test: 'password').set 'o8-|2}79'
 # browser.button(name: 'commit').click
